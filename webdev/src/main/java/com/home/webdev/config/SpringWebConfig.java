21bdc6f01fd7bd8dba3a2e6de1a69ea0181fe420
hunghuynh9822 <huuhung9822@gmail.com>
2018-06-29 08:56:32 +0700

    Add WebConfig for Internationalization

--- webdev/src/main/java/com/home/webdev/config/SpringWebConfig.java
+++ webdev/src/main/java/com/home/webdev/config/SpringWebConfig.java
@@ -1,31 +1,72 @@
 package com.home.webdev.config;
 
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+import com.home.webdev.interceptor.UrlLocaleInterceptor;
+
 @EnableWebMvc
 @Configuration
-@ComponentScan({"com.home.webdev"})
-public class SpringWebConfig extends WebMvcConfigurerAdapter{
+@ComponentScan({ "com.home.webdev" })
+public class SpringWebConfig extends WebMvcConfigurerAdapter {
+	private static final Charset UTF8 = Charset.forName("UTF-8");
+	// resources của web
 	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry)
-	{
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 	}
-	
+
 	@Bean
-	public InternalResourceViewResolver viewResolver()
-	{
+	public InternalResourceViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 		viewResolver.setViewClass(JstlView.class);
 		viewResolver.setPrefix("/views/");
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
 	}
+
+	// Them vào cho Da ngon ngu
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	// Thông tin Locale trên URL
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+
+		UrlLocaleInterceptor localeInterceptor = new UrlLocaleInterceptor();
+
+		registry.addInterceptor(localeInterceptor).addPathPatterns("/en/*", "/en/*/*", "/vi/*", "/vn/*/*");
+	}
+
+	// To solver URL like:
+	// /SpringMVCInternationalization/en/login2
+	// /SpringMVCInternationalization/vi/login2
+	// /SpringMVCInternationalization/fr/login2
+	
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		StringHttpMessageConverter stringConverter = new StringHttpMessageConverter();
+		stringConverter.setSupportedMediaTypes(Arrays.asList(new MediaType("text", "plain", UTF8)));
+		converters.add(stringConverter);
+
+		// Add other converters ...
+	}
+
 }
