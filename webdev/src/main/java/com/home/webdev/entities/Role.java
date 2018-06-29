02c2c0d55753a0bfde3c4930b136dd806a550dba
hunghuynh9822 <huuhung9822@gmail.com>
2018-06-29 11:23:05 +0700

    Add entities

---
+++ webdev/src/main/java/com/home/webdev/entities/Role.java
@@ -0,0 +1,37 @@
+package com.home.webdev.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table (name="role")
+public class Role {
+	@Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "role_id", nullable = false)
+	private Integer id;
+	
+	@Column(name = "role_name", nullable = false)
+	private String name;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Role(Integer id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+}
---
+++ webdev/src/main/java/com/home/webdev/entities/User.java
@@ -0,0 +1,128 @@
+package com.home.webdev.entities;
+
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+@Entity
+@Table(name ="user")
+public class User {
+	@Id
+	@Column(name = "username", unique = true, nullable = false)
+	private String username;
+	
+	@Column( name ="password", nullable = false)
+	private String password;
+	
+	@Column (name = "fullname",nullable = false)
+	private String fullname;
+	
+	@Column(name = "email", nullable = false)
+	private String email;
+	
+	@Column(name ="address", nullable = false)
+	private String address;
+	
+	@Column(name = "phone",nullable = false)
+	private String phone;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name = "dayofbirth", nullable = false)
+	private Date dayofbirth;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="role_id", nullable=false)
+	private Role role;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Date getDayofbirth() {
+		return dayofbirth;
+	}
+
+	public void setDayofbirth(Date dayofbirth) {
+		this.dayofbirth = dayofbirth;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public User(String username, String password, String fullname, String email, String address, String phone,
+			Date dayofbirth, Role role) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.fullname = fullname;
+		this.email = email;
+		this.address = address;
+		this.phone = phone;
+		this.dayofbirth = dayofbirth;
+		this.role = role;
+	}
+
+	private User() {
+		super();
+	}
+	
+	
+}
